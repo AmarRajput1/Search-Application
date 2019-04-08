c7a1586ee9cb167369825156748929daed7ed81c
AmarRajput1 <49390909+AmarRajput1@users.noreply.github.com>
2019-04-08 09:25:51 +0100

    Add files via upload

---
+++ ResultsTableSchema.sql
@@ -0,0 +1,19 @@
+USE [SearchResults]
+GO
+
+/****** Object:  Table [dbo].[ResultsTable]    Script Date: 04/08/2019 08:50:00 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[ResultsTable](
+	[NAME] [nvarchar](50) NULL,
+	[EMPLOYEE NUMBER] [nvarchar](50) NULL,
+	[GENDER] [nvarchar](50) NULL,
+	[SALARY] [real] NULL
+) ON [PRIMARY]
+
+GO
+
